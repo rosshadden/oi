29c7d783ed515e1fa2fcc1e12b7a400235db4d56
Ross Hadden <rosshadden@gmail.com>
2026-06-23 23:19:42 -0400

    feat: added octal, binary, and hex numbers

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,6 +15,9 @@ pub enum Token {
 	#[regex(r"(true|false)", |lex| lex.slice().parse().ok())]
 	Bool(bool),
 	#[regex(r"[0-9][0-9_]*", |lex| lex.slice().replace('_', "").parse().ok())]
+	#[regex(r"0[xX][0-9a-fA-F][0-9a-fA-F_]*", |lex| i32::from_str_radix(&lex.slice()[2..].replace('_', ""), 16).ok())]
+	#[regex(r"0[bB][01][01_]*", |lex| i32::from_str_radix(&lex.slice()[2..].replace('_', ""), 2).ok())]
+	#[regex(r"0[oO][0-7][0-7_]*", |lex| i32::from_str_radix(&lex.slice()[2..].replace('_', ""), 8).ok())]
 	Int(i32),
 	#[regex(r"[0-9][0-9_]*\.[0-9][0-9_]*", |lex| Some(lex.slice().replace('_', "")))]
 	Float(String),
--- tests/compiler/numbers.rs
+++ tests/compiler/numbers.rs
@@ -1,12 +1,36 @@
 use crate::helpers::*;
 
 #[test]
-fn num_seperators() {
+fn int() {
+	check("123", "123");
 	check("1_000_000_000", "1000000000");
 	check("1_2_3_4_5", "12345");
+}
+
+#[test]
+fn float() {
+	check("123.0", "123.0");
+	check("123.0000", "123.0");
 	check("10_000.22", "10000.22");
-	// TODO: add back once I add binary/octal/hex numbers
-	// check("0b1_1111_1111", "0b111111111");
-	// check("0o7_5_5", "0o755");
+}
+
+#[test]
+fn hex() {
+	check("0x7B", "123");
+	check("0xFF", "255");
+	check("0xFF_00", "65280");
+	// TODO: add once we support 64-bit numbers
 	// check("0xFF80_0000_0000_0000", "0xFF80000000000000");
+}
+
+#[test]
+fn binary() {
+	check("0b01111011", "123");
+	check("0b1_1111_1111", "511");
+}
+
+#[test]
+fn octal() {
+	check("0o173", "123");
+	check("0o7_5_5", "493");
 }
