4f9e309ee92e49fad82510c27749db757d12ec84
Ross Hadden <rosshadden@gmail.com>
2026-07-01 13:20:42 -0400

    test: enum shorthand

--- tests/compiler/types/enums.rs
+++ tests/compiler/types/enums.rs
@@ -96,3 +96,74 @@ fn unknown_variant() {
 	let err = fail("enum Color { red green blue }\nColor.purple");
 	assert!(err.contains("no variant `purple`"), "got: {err}");
 }
+
+#[test]
+fn shorthand_in_assignment() {
+	check(
+		"enum Color { red green blue }\nmut c := Color.green\nc = .red\nc",
+		"red",
+	);
+}
+
+#[test]
+fn shorthand_in_annotated_binding() {
+	check("enum Color { red green blue }\nc Color := .blue\nc", "blue");
+}
+
+#[test]
+fn shorthand_in_comparison() {
+	check(
+		"enum Color { red green blue }\nc := Color.red\nc == .red",
+		"true",
+	);
+	check(
+		"enum Color { red green blue }\nc := Color.red\nc != .blue",
+		"true",
+	);
+}
+
+#[test]
+fn shorthand_in_match() {
+	check(
+		indoc! {r#"
+			enum Color { red green blue }
+			c := Color.green
+			match c {
+				.red { "r" }
+				.green { "g" }
+				else { "?" }
+			}
+		"#},
+		"g",
+	);
+}
+
+#[test]
+fn shorthand_in_struct_field() {
+	check(
+		"enum Stat { health mana stamina }
+		struct User { s Stat }
+		u := User{ s: .mana }
+		u.s",
+		"mana",
+	);
+	check(
+		"enum Stat { health mana stamina }
+		struct User { s Stat }
+		u := User{ .stamina }
+		u.s",
+		"stamina",
+	);
+}
+
+#[test]
+fn shorthand_unknown_variant() {
+	let err = fail("enum Color { red green blue }\nc := Color.red\nc == .purple");
+	assert!(err.contains("no variant `purple`"), "got: {err}");
+}
+
+#[test]
+fn shorthand_without_context_errors() {
+	let err = fail("enum Color { red green blue }\n.red");
+	assert!(err.contains("cannot infer the enum type"), "got: {err}");
+}
