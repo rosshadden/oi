03c28d291733a22448fab50293c3fdd89d17eac2
Ross Hadden <rosshadden@gmail.com>
2026-07-15 22:52:59 -0400

    test: fixed serve test to make it kill zola when done

--- tests/cli/serve.rs
+++ tests/cli/serve.rs
@@ -1,3 +1,4 @@
+use std::os::unix::process::CommandExt;
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
@@ -19,9 +20,15 @@ fn serve_spawns_and_stays_alive() {
 		.args(["serve"])
 		.stdout(Stdio::null())
 		.stderr(Stdio::null())
+		.process_group(0) // own group, so we can kill zola alongside it below
 		.spawn()
 		.unwrap();
 	thread::sleep(Duration::from_millis(200));
 	assert!(child.try_wait().unwrap().is_none(), "serve exited early");
-	child.kill().unwrap();
+
+	Command::new("kill")
+		.args(["-KILL", "--", &format!("-{}", child.id())])
+		.status()
+		.unwrap();
+	child.wait().unwrap();
 }
